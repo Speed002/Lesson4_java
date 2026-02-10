a6bb0a05b7505b5fbe03194fbd9c9e5f5d6dfcb6
Wasswa Speed <speedwasswa02@gmail.com>
2026-02-10 17:20:04 +0300

    This is my first commit

---
+++ Lesson4.java
@@ -0,0 +1,11 @@
+// ---------- FIRST TIME SETUP ----------
+// git config --global user.name "Your Name"
+// git config --global user.email "you@email.com"
+
+// // ---------- ON MY SIDE (PROJECT OWNER) ----------
+// git init                          // initialize repo
+// git status                        // check file status (VERY IMPORTANT)
+// git remote add origin <repo_url>  // connect to remote repo
+// git add .                         // stage all changes
+// git commit -m "initial commit"    // commit changes
+// git push -u origin main           // push & set upstream
